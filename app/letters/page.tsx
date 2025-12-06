ffa2c45307c9df458c2cb713ac8d1beefe6ce2b0
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-12-06 20:51:12 +0700

    modify leaderboards theme

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -1,5 +1,77 @@
+"use client"
+import './page.css';
 export default function Letters() {
     return (
-        <div>Letters</div>
+        <div className="hiragana-keyboard">
+            <div className="row">
+                <div className="char-box">
+                    <span className="hiragana">あ</span>
+                    <span className="romaji">a</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">い</span>
+                    <span className="romaji">i</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">う</span>
+                    <span className="romaji">u</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">え</span>
+                    <span className="romaji">e</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">お</span>
+                    <span className="romaji">o</span>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="char-box">
+                    <span className="hiragana">か</span>
+                    <span className="romaji">ka</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">き</span>
+                    <span className="romaji">ki</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">く</span>
+                    <span className="romaji">ku</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">け</span>
+                    <span className="romaji">ke</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">こ</span>
+                    <span className="romaji">ko</span>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="char-box">
+                    <span className="hiragana">さ</span>
+                    <span className="romaji">sa</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">し</span>
+                    <span className="romaji">shi</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">す</span>
+                    <span className="romaji">su</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">せ</span>
+                    <span className="romaji">se</span>
+                </div>
+                <div className="char-box">
+                    <span className="hiragana">そ</span>
+                    <span className="romaji">so</span>
+                </div>
+            </div>
+
+        </div>
     )
 }
