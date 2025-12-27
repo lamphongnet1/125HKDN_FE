e4d70c289af2f03d7f31628bf91116e01509ff6f
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-12-27 14:35:54 +0700

    stupid IDE

--- app/learn/layout.tsx
+++ app/learn/layout.tsx
@@ -1,20 +1,20 @@
 import './globals.css';
 import { Sidebar } from './components/Sidebar';
-import {RightSidebar} from './components/RightSidebar';
+import { RightSidebar } from './components/RightSidebar';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="vi">
       <body>
-      <div className="flex"> {/* Bỏ h-screen để trang web có thể cuộn tự do */}
-          <Sidebar />
-          <div className="flex-1 flex justify-center ml-72">
-            <div className="w-[692px]">
-              {children}
-            </div>         
-          </div> 
-          <RightSidebar />  
-        </div>
+        <div className="flex"> {/* Bỏ h-screen để trang web có thể cuộn tự do */}
+          <Sidebar />
+          <div className="flex-1 flex justify-center ml-72">
+            <div className="w-[692px]">
+              {children}
+            </div>
+          </div>
+          <RightSidebar />
+        </div>
       </body>
     </html>
   );
-}
+}//bug
